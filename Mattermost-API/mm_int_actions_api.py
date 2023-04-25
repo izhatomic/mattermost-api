a9f3ff0dec99873a74b7801f6134331a25692f8c
Alexander Zarin <adamant545@gmail.com>
2023-04-26 00:37:19 +0400

    Correct the class name

--- Mattermost-API/mm_int_actions_api.py
+++ Mattermost-API/mm_int_actions_api.py
@@ -2,7 +2,7 @@ from typing import Union, List, Dict
 from Mattermost_Base import Base
 
 
-class Integration_Actions(Base):
+class IntegrationActions(Base):
     def __init__(self, token: str, server_url: str):
         super().__init__(token, server_url)
         self.api_url = f"{self.base_url}/actions/dialogs"
