fac148953b23660095741b42fbf46e0998ea74f2
Alexander Zarin <adamant545@gmail.com>
2023-05-01 15:56:46 +0400

    Create the download_report function

--- Mattermost-API/mm_compliance_api.py
+++ Mattermost-API/mm_compliance_api.py
@@ -44,7 +44,7 @@ class Compliance(Base):
 
         return self.request(url, request_type='GET')
 
-    def get_report(self, report_id: str):
+    def get_report(self, report_id: str) -> dict:
         """
         Get a compliance reports previously created.
 
@@ -52,6 +52,23 @@ class Compliance(Base):
 
         :param report_id: Compliance report GUID
         :return: Compliance report retrieval successful
+        """
+
+        url = f"{self.api_url}/reports/{report_id}"
+
+        self.reset()
+        self.add_to_json('report_id', report_id)
+
+        return self.request(url, request_type='GET')
+
+    def download_report(self, report_id: str) -> dict:
+        """
+        Download the full contents of a report as a file.
+
+        Must have manage_system permission.
+
+        :param report_id: Compliance report GUID
+        :return: The compliance report file
         """
 
         url = f"{self.api_url}/reports/{report_id}"
