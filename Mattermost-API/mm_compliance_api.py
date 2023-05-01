dd1adb46749fde65d39d5486e51c6f008a544138
Alexander Zarin <adamant545@gmail.com>
2023-05-01 15:23:15 +0400

    Create the get_reports function

--- Mattermost-API/mm_compliance_api.py
+++ Mattermost-API/mm_compliance_api.py
@@ -13,11 +13,34 @@ class Compliance(Base):
 
         Must have manage_system permission.
 
-        :return:Compliance report creation successfull
+        :return: Compliance report creation successfull
         """
 
         url = f"{self.api_url}/reports"
 
         self.reset()
 
-        return self.request(url, request_type='POST', body=True)
+        return self.request(url, request_type='POST')
+
+    def get_reports(self, page: int, per_page: int) -> dict:
+        """
+        Get a list of compliance reports previously created by page,
+        selected with page and per_page query parameters.
+
+        Must have manage_system permission.
+
+        :param page: The page to select.
+        :param per_page: The number of reports per page.
+        :return: Compliance reports retrieval successful
+        """
+
+        url = f"{self.api_url}/reports"
+
+        self.reset()
+        if page is not None:
+            self.add_to_json('page', page)
+        if per_page is not None:
+            self.add_to_json('per_page', per_page)
+
+        return self.request(url, request_type='GET')
+
