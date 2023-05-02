87ccc211a1b817c6c6b74b34c332cfe34e5e11ed
Alexander Zarin <adamant545@gmail.com>
2023-05-02 21:29:08 +0400

    Create the list_export_files function

--- Mattermost-API/mm_exports_api.py
+++ Mattermost-API/mm_exports_api.py
@@ -1,1 +1,26 @@
+from typing import Union, List, Dict
+from Mattermost_Base import Base
+
+
+class Exports(Base):
+    def __init__(self, token: str, server_url: str):
+        super().__init__(token, server_url)
+        self.api_url = f"{self.base_url}/exports"
+
+    def list_export_files(self) -> dict:
+        """
+        Lists all available export files.
+
+        Minimum server version: 5.33
+        Must have manage_system permissions.
+
+        :return: List of all available export files
+        """
+
+        url = f"{self.api_url}/"
+
+        self.reset()
+
+        return self.request(url, request_type='GET')
+
 
