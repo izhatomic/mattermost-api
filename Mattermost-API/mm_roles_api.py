876d70d28548320bb8a3d23799fc03557c686b1d
Alexander Zarin <adamant545@gmail.com>
2023-11-07 00:35:02 +0400

    Define the get_a_list_of_roles_by_name function.

--- Mattermost-API/mm_roles_api.py
+++ Mattermost-API/mm_roles_api.py
@@ -51,7 +51,7 @@ class Roles(Base):
 
         return self.request(url, request_type='GET')
 
-    def patch_a_role(self, role_id: str) -> dict:
+    def patch_a_role(self, role_id: str, permissions: list[str]) -> dict:
         """
         Partially update a role by providing only the fields you want to update.
         Omitted fields will not be updated.
@@ -62,11 +62,31 @@ class Roles(Base):
         manage_system permission is required.
 
         :param role_id: Role GUID.
+        :param permissions: The permissions the role should grant.
         :return: Role patch info.
         """
         url = f"{self.api_url}/name/{role_id}/patch"
         self.reset()
+        self.add_application_json_header()
+        if permissions is not None:
+            self.add_to_json('permissions', permissions)
 
-        return self.request(url, request_type='GET')
+        return self.request(url, request_type='PUT', body=True)
 
+    def get_a_list_of_roles_by_name(self, roles_list: list[str]) -> dict:
+        """
+        Get a list of roles from their names.
 
+        Minimum server version: 4.9
+        Requires an active session but no other permissions.
+
+        :param roles_list: List of role names.
+        :return: Role list info.
+        """
+        url = f"{self.api_url}/names"
+        self.reset()
+        self.add_application_json_header()
+        if roles_list is not None:
+            self.add_to_json('roles_list', roles_list)
+
+        return self.request(url, request_type='POST', body=True)
