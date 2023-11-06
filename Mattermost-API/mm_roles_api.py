7cf602978c50bfca2987341ca1f4711816ba2d77
Alexander Zarin <adamant545@gmail.com>
2023-11-06 23:26:13 +0400

    Define the get_a_role_from_name function.

--- Mattermost-API/mm_roles_api.py
+++ Mattermost-API/mm_roles_api.py
@@ -35,3 +35,18 @@ class Roles(Base):
         self.reset()
 
         return self.request(url, request_type='GET')
+
+    def get_a_role_from_name(self, role_name: str) -> dict:
+        """
+        Get a role from the provided role name.
+
+        Minimum server version: 4.9
+        Requires an active session but no other permissions.
+
+        :param role_name: Role name.
+        :return: Role retrieval info.
+        """
+        url = f"{self.api_url}/name/{role_name}"
+        self.reset()
+
+        return self.request(url, request_type='GET')
