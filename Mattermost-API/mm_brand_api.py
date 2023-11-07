859919cadd5bcb62cedfe76a59c83b4ea0d3d838
Alexander Zarin <adamant545@gmail.com>
2023-11-07 23:38:23 +0400

    Define the get_brand_image.py function.

--- Mattermost-API/mm_brand_api.py
+++ Mattermost-API/mm_brand_api.py
@@ -7,3 +7,16 @@ class Brand(Base):
         super().__init__(token, server_url)
         self.api_url = f"{self.base_url}/brand"
 
+    def get_brand_image(self) -> dict:
+        """
+        Get the previously uploaded brand image.
+        Returns 404 if no brand image has been uploaded.
+
+        No permission required.
+
+        :return: Brand image retrieval info.
+        """
+        url = f"{self.api_url}/image"
+        self.reset()
+
+        return self.request(url, request_type='GET')
