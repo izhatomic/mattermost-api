e1dbf3a2885cc199276427f059d7fe800bc4f639
Alexander Zarin <adamant545@gmail.com>
2023-11-07 22:31:08 +0400

    Define the get_jobs_of_given_type function.

--- Mattermost-API/mm_jobs_api.py
+++ Mattermost-API/mm_jobs_api.py
@@ -97,3 +97,29 @@ class Jobs(Base):
         self.reset()
 
         return self.request(url, request_type='POST')
+
+    def get_jobs_of_given_type(self,
+                               type: str,
+                               page: int,
+                               per_page: int) -> dict:
+        """
+        Get a page of jobs of the given type.
+        Use the query parameters to modify the behaviour of this endpoint.
+
+        Minimum server version: 4.1
+        Must have manage_jobs permission.
+
+        :param type: Job type.
+        :param page: Default: 0. The page to select.
+        :param per_page: Default: 60. The number of jobs per page.
+        :return: Job list status.
+        """
+        url = f"{self.api_url}/type/{type}"
+        self.reset()
+        self.add_application_json_header()
+        if page is not None:
+            self.add_to_json('page', page)
+        if per_page is not None:
+            self.add_to_json('per_page', per_page)
+
+        return self.request(url, request_type='GET', body=True)
