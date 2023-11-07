24523cf159e24df01bd33e2775c1e4258ac06de3
Alexander Zarin <adamant545@gmail.com>
2023-11-07 20:40:06 +0400

    Define the install_plugin_from_url function.

--- Mattermost-API/mm_plugins_api.py
+++ Mattermost-API/mm_plugins_api.py
@@ -43,4 +43,25 @@ class Plugins(Base):
         url = f"{self.api_url}/"
         self.reset()
 
-        return self.request(url, request_type='POST', body=True)
+        return self.request(url, request_type='GET')
+
+    def install_plugin_from_url(self,
+                                plugin_download_url: str,
+                                force: str) -> dict:
+        """
+        Supply a URL to a plugin compressed in a .tar.gz file.
+        Plugins must be enabled in the server's config settings.
+
+        Minimum server version: 5.14
+        Must have manage_system permission.
+
+        :return: Plugin installation info.
+        """
+        url = f"{self.api_url}/"
+        self.reset()
+        self.add_application_json_header()
+        self.add_to_json('plugin_download_url', plugin_download_url)
+        if force is not None:
+            self.add_to_json('force', force)
+
+        return self.request(url, request_type='GET', body=True)
