5145456d871e089c9e6f15741ac9fe93d1660cdc
Alexander Zarin <adamant545@gmail.com>
2023-11-08 21:49:19 +0400

    Define the get_policies_applied_to_user_chnls function

--- Mattermost-API/mm_data_retention_api.py
+++ Mattermost-API/mm_data_retention_api.py
@@ -33,3 +33,26 @@ class DataRetention(Base):
             self.add_to_json('per_page', per_page)
 
         return self.request(url, request_type='GET', body=True)
+
+    def get_policies_applied_to_user_chnls(self, user_id: str, page: int, per_page: int) -> dict:
+        """
+        Gets the policies which are applied to the all of the channels to which a user belongs.
+
+        Minimum server version: 5.35
+        Must be logged in as the user or have the manage_system permission.
+        Requires an E20 license.
+
+        :param user_id:	The ID of the user. This can also be "me" which will point to the current user.
+        :param page: Default: 0. The page to select.
+        :param per_page: Default: 60. The number of policies per page. There is a maximum limit of 200 per page.
+        :return: Retention policy channels info.
+        """
+        url = f"{self.base_url}/users/{user_id}/data_retention/channel_policies"
+        self.reset()
+        self.add_application_json_header()
+        if page is not None:
+            self.add_to_json('page', page)
+        if per_page is not None:
+            self.add_to_json('per_page', per_page)
+
+        return self.request(url, request_type='GET', body=True)
