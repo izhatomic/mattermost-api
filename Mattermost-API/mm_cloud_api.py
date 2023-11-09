6ecec361ac1a4b7b8a9b3d2341c867334020f1eb
Alexander Zarin <adamant545@gmail.com>
2023-11-09 23:38:42 +0400

    Define the create_customer_setup_payment_intent function

--- Mattermost-API/mm_cloud_api.py
+++ Mattermost-API/mm_cloud_api.py
@@ -38,3 +38,19 @@ class Cloud(Base):
         self.reset()
 
         return self.request(url, request_type='GET')
+
+    def create_customer_setup_payment_intent(self) -> dict:
+        """
+        Creates a customer setup payment intent for the given Mattermost cloud installation.
+
+        Minimum server version: 5.28
+        Must have manage_system permission and be licensed for Cloud.
+        Note:: This is intended for internal use and is subject to change.
+
+        :return: Payment setup info.
+        """
+
+        url = f"{self.api_url}/payment"
+        self.reset()
+
+        return self.request(url, request_type='POST')
