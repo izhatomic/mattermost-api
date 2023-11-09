12a6c13003a6b286852b57ef0c11215b535bb8bb
Alexander Zarin <adamant545@gmail.com>
2023-11-09 23:33:26 +0400

    Define the get_cloud_workspace_limits function

--- Mattermost-API/mm_cloud_api.py
+++ Mattermost-API/mm_cloud_api.py
@@ -6,3 +6,19 @@ class Cloud(Base):
     def __init__(self, token: str, server_url: str):
         super().__init__(token, server_url)
         self.api_url = f"{self.base_url}/cloud"
+
+    def get_cloud_workspace_limits(self) -> dict:
+        """
+        Retrieve any cloud workspace limits applicable to this instance.
+
+        Minimum server version: 7.0
+        Must be authenticated and be licensed for Cloud.
+        Note: This is intended for internal use and is subject to change.
+
+        :return: Cloud workspace limits info.
+        """
+
+        url = f"{self.api_url}/limits"
+        self.reset()
+
+        return self.request(url, request_type='GET')
