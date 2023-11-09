f7e4984a42b15a0c384e7b61bfd3e9fc59de21e0
Alexander Zarin <adamant545@gmail.com>
2023-11-09 23:50:05 +0400

    Define the update_cloud_customer function

--- Mattermost-API/mm_cloud_api.py
+++ Mattermost-API/mm_cloud_api.py
@@ -91,3 +91,39 @@ class Cloud(Base):
         self.reset()
 
         return self.request(url, request_type='GET')
+
+    def update_cloud_customer(self,
+                              name: str,
+                              email: str,
+                              contact_first_name: str,
+                              contact_last_name: str,
+                              num_employees: str) -> dict:
+        """
+        Updates the customer information for the Mattermost Cloud customer bound to this installation.
+
+        Minimum server version: 5.29
+        Must have manage_system permission and be licensed for Cloud.
+        Note: This is intended for internal use and is subject to change.
+
+        :param name: The name to update.
+        :param email: The email to update.
+        :param contact_first_name: The contact first name to update.
+        :param contact_last_name: The contact last name to update.
+        :param num_employees: The number of employees.
+        :return: Cloud customer updating info.
+        """
+        url = f"{self.api_url}/customer"
+        self.reset()
+        self.add_application_json_header()
+        if name is not None:
+            self.add_to_json('name', name)
+        if email is not None:
+            self.add_to_json('email', email)
+        if contact_first_name is not None:
+            self.add_to_json('contact_first_name', contact_first_name)
+        if contact_last_name is not None:
+            self.add_to_json('contact_last_name', contact_last_name)
+        if num_employees is not None:
+            self.add_to_json('num_employees', num_employees)
+
+        return self.request(url, request_type='PUT', body=True)
