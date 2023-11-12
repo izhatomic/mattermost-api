0cedd66882050a2ff093ba2504e89da40c9ce392
Alexander Zarin <adamant545@gmail.com>
2023-11-12 20:27:02 +0400

    Define the delete_link_for_ldap_group function

--- Mattermost-API/mm_groups_api.py
+++ Mattermost-API/mm_groups_api.py
@@ -6,3 +6,20 @@ class Cloud(Base):
     def __init__(self, token: str, server_url: str):
         super().__init__(token, server_url)
         self.api_url = f"{self.base_url}/groups"
+
+    def delete_link_for_ldap_group(self,remote_id:str) ->dict:
+        """
+        Deletes a link for LDAP group.
+
+        Minimum server version: 5.11
+        Must have manage_system permission.
+
+        :param remote_id: Group GUID.
+        :return: Cloud workspace limits info.
+        """
+        url = f"{self.base_url}/ldap/groups/{remote_id}/link"
+        self.reset()
+
+        return self.request(url, request_type='DEL')
+
+
