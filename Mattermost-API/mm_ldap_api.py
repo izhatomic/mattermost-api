287a1b7e09fa4c9929e618974cba6258c7918da9
Alexander Zarin <adamant545@gmail.com>
2023-11-13 18:51:42 +0400

    Create mm_ldap_api.py

---
+++ Mattermost-API/mm_ldap_api.py
@@ -0,0 +1,10 @@
+from typing import Union, List, Dict
+from Mattermost_Base import Base
+
+
+class Ldap(Base):
+    def __init__(self, token: str, server_url: str):
+        super().__init__(token, server_url)
+        self.api_url = f"{self.base_url}/ldap"
+
+    def (self) -> dict:
