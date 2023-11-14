4b8ef79ba1bc8e70a4364aaba0734c0da24f7957
Alexander Zarin <adamant545@gmail.com>
2023-11-14 21:16:28 +0400

    Define the migrate_user_acc_auth_type_to_saml function

--- Mattermost-API/mm_saml_api.py
+++ Mattermost-API/mm_saml_api.py
@@ -8,3 +8,25 @@ class saml(Base):
         self.api_url = f"{self.base_url}/saml"
 
 
+    def migrate_user_acc_auth_type_to_saml(self, from:str, matches: dict, auto: bool) -> dict:
+        """
+        Migrates accounts from one authentication provider to another.
+        For example, you can upgrade your authentication provider from email to SAML.
+        Minimum server version: 5.28
+        Must have manage_system permission.
+        :param from: The current authentication type for the matched users.
+        :param matches: Users map.
+        :param auto: Toggle auto migration.
+        :return: User account migration info.
+        """
+        url = f"{self.base_url}/users/migrate_auth/saml"
+        self.reset()
+        self.add_application_json_header()
+        if from is not None:
+            self.add_to_json('from', from)
+        if matches is not None:
+            self.add_to_json('matches', matches)
+        if auto is not None:
+            self.add_to_json('auto', auto)
+
+        return self.request(url, request_type='POST', body=True)
