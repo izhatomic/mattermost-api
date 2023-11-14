25d95bd5d2b2401cbf403cbb919a8e3289fe23d6
Alexander Zarin <adamant545@gmail.com>
2023-11-14 21:26:44 +0400

    Define the upload_private_key function

--- Mattermost-API/mm_saml_api.py
+++ Mattermost-API/mm_saml_api.py
@@ -111,3 +111,18 @@ class saml(Base):
         self.reset()
 
         return self.request(url, request_type='DEL')
+
+    def upload_private_key(self, certificate: str) -> dict:
+        """
+        Upload the private key to be used for encryption with your SAML configuration. The server will pick a hard-coded filename for the PrivateKeyFile setting in your config.json.
+        Must have sysconsole_write_authentication permission.
+        :param certificate: The private key file
+        :return: SAML certificate info.
+        """
+        url = f"{self.api_url}/certificate/private"
+        self.reset()
+        self.add_application_json_header()
+        if certificate is not None:
+            self.add_to_json('certificate', certificate)
+
+        return self.request(url, request_type='POST', body=True)
