f7e1b060a52ef3605f2e3a825cfba5191fbbff6e
Alexander Zarin <adamant545@gmail.com>
2023-11-14 22:12:43 +0400

    Create mm_oauth_api.py

---
+++ Mattermost-API/mm_oauth_api.py
@@ -0,0 +1,8 @@
+from typing import Union, List, Dict
+from Mattermost_Base import Base
+
+
+class Oauth(Base):
+    def __init__(self, token: str, server_url: str):
+        super().__init__(token, server_url)
+        self.api_url = f"{self.base_url}/oauth/apps"
