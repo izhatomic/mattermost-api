5d2dbb8e31aed83a69ce534e9770f08c47cde48b
Alexander Zarin <adamant545@gmail.com>
2023-11-15 23:27:27 +0400

    Define the get_list_of_custom_emoji function

--- Mattermost-API/mm_emoji_api.py
+++ Mattermost-API/mm_emoji_api.py
@@ -29,3 +29,33 @@ class Emoji(Base):
         self.add_to_json('emoji', emoji)
 
         return self.request(url, request_type='POST', body=True)
+
+    def get_list_of_custom_emoji(self,
+                                 page: int,
+                                 per_page: int,
+                                 sort: str) -> dict:
+        """
+        Get a page of metadata for custom emoji on the system.
+        Since server version 4.7, sort using the sort query parameter.
+
+        Must be authenticated.
+
+        :param page: Default: 0. The page to select.
+        :param per_page: Default: 60. The number of emojis per page.
+        :param sort: Default: "". Either blank for no sorting or "name" to sort by emoji names.
+        Minimum server version for sorting is 4.7.
+        :return: Emoji list retrieval info.
+        """
+
+        url = f"{self.api_url}/"
+
+        self.reset()
+        self.add_application_json_header()
+        if page is not None:
+            self.add_to_json('page', page)
+        if per_page is not None:
+            self.add_to_json('per_page', per_page)
+        if sort is not None:
+            self.add_to_json('sort', sort)
+
+        return self.request(url, request_type='GET', body=True)
