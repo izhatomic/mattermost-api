aff27ae72f11b71d1cc04b5393d11acc1db2f6e1
Alexander Zarin <adamant545@gmail.com>
2023-11-15 23:11:33 +0400

    Create mm_emoji_api.py

---
+++ Mattermost-API/mm_emoji_api.py
@@ -0,0 +1,9 @@
+from typing import Union, List, Dict
+from Mattermost_Base import Base
+
+
+class Emoji(Base):
+    def __init__(self, token: str, server_url: str):
+        super().__init__(token, server_url)
+        self.api_url = f"{self.base_url}/emoji"
+
