5dda921e8972079198905dc0f931625585ca64f3
Alexander Zarin <adamant545@gmail.com>
2023-11-17 00:29:42 +0400

    Define the get_user_status function

--- Mattermost-API/mm_status_api.py
+++ Mattermost-API/mm_status_api.py
@@ -6,3 +6,19 @@ class Status(Base):
     def __init__(self, token: str, server_url: str):
         super().__init__(token, server_url)
         self.api_url = f"{self.base_url}/users"
+
+    def get_user_status(self, user_id: str) -> dict:
+        """
+        Get user status by id from the server.
+
+        Must be authenticated.
+
+        :param user_id: User ID.
+        :return: User status retrieval info.
+        """
+
+        url = f"{self.api_url}/{user_id}/status"
+
+        self.reset()
+
+        return self.request(url, request_type='GET')
