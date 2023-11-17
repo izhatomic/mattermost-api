ccc6d08f7fa713ca50df7575bee636e19496dc60
Alexander Zarin <adamant545@gmail.com>
2023-11-17 21:53:00 +0400

    Define the delete_user_preferences function

--- Mattermost-API/mm_prferences_api.py
+++ Mattermost-API/mm_prferences_api.py
@@ -54,6 +54,38 @@ class Preferences(Base):
 
         return self.request(url, request_type='PUT')
 
+    def delete_user_preferences(self,
+                                user_id: str,
+                                category: str,
+                                name: str,
+                                value: str) -> dict:
+        """
+        Lists the current user's stored preferences in the given category.
+
+        Must be logged in as the user being updated or have the edit_other_users permission.
+
+        :param user_id: User ID.
+        :param category: The category of a group of preferences.
+        :param name: The name of a group of preferences.
+        :param value: The value of a group of preferences.
+        :return: User preferences saving info.
+        """
+
+        url = f"{self.api_url}/{user_id}/preferences/delete"
+
+        self.reset()
+        self.add_application_json_header()
+        if user_id is not None:
+            self.add_to_json('user_id', user_id)
+        if category is not None:
+            self.add_to_json('category', category)
+        if name is not None:
+            self.add_to_json('name', name)
+        if value is not None:
+            self.add_to_json('value', value)
+
+        return self.request(url, request_type='POST')
+
     def list_user_preferences_by_category(self,
                                           user_id: str,
                                           category: str) -> dict:
@@ -67,11 +99,10 @@ class Preferences(Base):
         :return: User preferences saving info.
         """
 
-        url = f"{self.api_url}/{user_id}/preferences/delete"
+        url = f"{self.api_url}/{user_id}/preferences/{category}"
 
         self.reset()
-        self.add_application_json_header()
-        if category is not None:
-            self.add_to_json('category', category)
 
-        return self.request(url, request_type='POST')
+        return self.request(url, request_type='GET')
+
+    def
