2f1d68a2c175725e6eff53520be0b14a2e0fb5f7
Alexander Zarin <adamant545@gmail.com>
2023-11-17 21:05:06 +0400

    Define the get_user_preferences function

--- Mattermost-API/mm_prferences_api.py
+++ Mattermost-API/mm_prferences_api.py
@@ -7,4 +7,17 @@ class Preferences(Base):
         super().__init__(token, server_url)
         self.api_url = f"{self.base_url}/users"
 
+    def get_user_preferences(self, user_id: str) -> dict:
+        """
+        Get a list of the user's preferences.
 
+        Must be logged in as the user being updated or have the edit_other_users permission.
+
+        :param user_id: User ID.
+        :return: User preferences retrieval info.
+        """
+        url = f"{self.api_url}/{user_id}/preferences"
+
+        self.reset()
+
+        return self.request(url, request_type='GET')
