d1fc9c604c93a7d4e9e425ba17de7ae3373e37ab
Alexander Zarin <adamant545@gmail.com>
2023-11-17 21:25:08 +0400

    Define the save_user_preferences function

--- Mattermost-API/mm_prferences_api.py
+++ Mattermost-API/mm_prferences_api.py
@@ -21,3 +21,34 @@ class Preferences(Base):
         self.reset()
 
         return self.request(url, request_type='GET')
+
+    def save_user_preferences(self,
+                              user_id: str,
+                              category: str,
+                              name: str,
+                              value: str) -> dict:
+        """
+        Save a list of the user's preferences.
+
+        Must be logged in as the user being updated or have the edit_other_users permission.
+
+        :param user_id: User ID.
+        :param category: Category.
+        :param name: Name.
+        :param value: Value.
+        :return: User preferences saving info.
+        """
+        url = f"{self.api_url}/{user_id}/preferences"
+
+        self.reset()
+        self.add_application_json_header()
+        if user_id is not None:
+            self.add_to_json('user_id', user_id)
+        if category is not None:
+            self.add_to_json('category', category)
+        if name is not None:
+            self.add_to_json('name', name)
+        if value is not None:
+            self.add_to_json('value', value)
+
+        return self.request(url, request_type='PUT')
