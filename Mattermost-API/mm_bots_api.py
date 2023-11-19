e86cbc247f71d0d500dd7277f47752e54d73a33d
Alexander Zarin <adamant545@gmail.com>
2023-11-19 17:02:17 +0400

    Define the convert_user_into_bot function

--- Mattermost-API/mm_bots_api.py
+++ Mattermost-API/mm_bots_api.py
@@ -7,5 +7,21 @@ class Bots(Base):
         super().__init__(token, server_url)
         self.api_url = f"{self.base_url}/users"
 
+    def convert_user_into_bot(self,
+                              user_id: str) -> dict:
+        """
+        Convert a user into a bot.
 
+        Minimum server version: 5.26
 
+        Must have manage_system permission.
+
+        :param user_id: User GUID
+        :return: User conversion info
+        """
+
+        url = f"{self.api_url}/{user_id}/convert_to_bot"
+
+        self.reset()
+
+        return self.request(url, request_type='POST')
