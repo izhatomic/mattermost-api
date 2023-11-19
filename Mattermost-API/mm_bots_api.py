299d0ca3d32112e86e74857cb60990232b806f5a
Alexander Zarin <adamant545@gmail.com>
2023-11-19 18:46:20 +0400

    Define the create_bot function

--- Mattermost-API/mm_bots_api.py
+++ Mattermost-API/mm_bots_api.py
@@ -24,3 +24,32 @@ class Bots(Base):
         self.reset()
 
         return self.request(url, request_type='POST')
+
+    def create_bot(self,
+                   username: str,
+                   display_name: str,
+                   description: str) -> dict:
+        """
+        Create a new bot account on the system. Username is required.
+
+        Must have create_bot permission.
+
+        Minimum server version: 5.10
+
+        :param username: Bot's name
+        :param display_name: Bot's display name
+        :param description: Bot's description
+        :return: Bot creation info
+        """
+
+        url = f"{self.api_url}"
+
+        self.reset()
+        self.add_application_json_header()
+        self.add_to_json('username', username)
+        if display_name is not None:
+            self.add_to_json('display_name', display_name)
+        if description is not None:
+            self.add_to_json('description', description)
+
+        return self.request(url, request_type='POST', body=True)
