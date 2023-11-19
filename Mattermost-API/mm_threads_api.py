d91239733c00f5f110395ed8be41b0d1a8cada2a
Alexander Zarin <adamant545@gmail.com>
2023-11-19 14:42:40 +0400

    Define the get_threads_user_is_following function

--- Mattermost-API/mm_threads_api.py
+++ Mattermost-API/mm_threads_api.py
@@ -7,6 +7,54 @@ class Threads(Base):
         super().__init__(token, server_url)
         self.api_url = f"{self.base_url}/users"
 
+    def get_threads_user_is_following(self,
+                                      user_id: str,
+                                      team_id: str,
+                                      since: int,
+                                      deleted: bool,
+                                      extended: bool,
+                                      page: int,
+                                      pageSize: int,
+                                      totalsOnly: bool,
+                                      threadsOnly: bool):
+        """
+        Get all threads that user is following.
+
+        Minimum server version: 5.29
+        Must be logged in as the user or have edit_other_users permission.
+
+        :param user_id: The ID of the user. This can also be "me" which will point to the current user.
+        :param team_id: The ID of the team in which the thread is.
+        :param since: Since filters the threads based on their LastUpdateAt timestamp.
+        :param deleted: Default: false. Deleted will specify that even deleted threads should be returned (For mobile sync).
+        :param extended: Default: false. Extended will enrich the response with participant details.
+        :param page: Default: 0. Page specifies which part of the results to return, by PageSize.
+        :param pageSize: Default: 30. PageSize specifies the size of the returned chunk of results.
+        :param totalsOnly: Default: false. Setting this to true will only return the total counts.
+        :param threadsOnly: Default: false. Setting this to true will only return threads.
+        :return: User's threads retrieval info.
+        """
+
+        url = f"{self.api_url}/{user_id}/teams/{team_id}/threads"
+
+        self.reset()
+        self.add_application_json_header()
+        if since is not None:
+            self.add_to_json('since', since)
+        if deleted is not None:
+            self.add_to_json('deleted', deleted)
+        if extended is not None:
+            self.add_to_json('extended', extended)
+        if page is not None:
+            self.add_to_json('page', page)
+        if pageSize is not None:
+            self.add_to_json('pageSize', pageSize)
+        if totalsOnly is not None:
+            self.add_to_json('totalsOnly', totalsOnly)
+        if threadsOnly is not None:
+            self.add_to_json('threadsOnly', threadsOnly)
+
+        return self.request(url, request_type='GET', body=True)
     def get_unread_mention_counts_from_followed_threads(self,
                                                         user_id: str,
                                                         team_id: str) -> dict:
@@ -18,7 +66,7 @@ class Threads(Base):
 
         :param user_id: The ID of the user. This can also be "me" which will point to the current user.
         :param team_id: The ID of the team in which the thread is.
-        :return: Upload creation successful.
+        :return: Get process info.
         """
 
         url = f"{self.api_url}/{user_id}/teams/{team_id}/threads/mention_counts"
@@ -26,3 +74,5 @@ class Threads(Base):
         self.reset()
 
         return self.request(url, request_type='GET')
+
+
