5e4be7eb7a3ea84233a9855d795deb21d54a1141
amkuznetsov <amkuznetsov@sberdevices.ru>
2023-11-19 18:55:20 +0300

    Fix SharedChannels

--- Mattermost-API/mm_shared_channels_api.py
+++ Mattermost-API/mm_shared_channels_api.py
@@ -2,7 +2,7 @@ from typing import Union, List, Dict
 from Mattermost_Base import Base
 
 
-class Shared_channels(Base):
+class SharedChannels(Base):
     def __init__(self, token: str, server_url: str):
         super().__init__(token, server_url)
         self.api_url = f"{self.base_url}/sharedchannels"
