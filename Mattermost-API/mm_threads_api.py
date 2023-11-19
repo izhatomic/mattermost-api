b746e7a2d4a75c0c7cf5c539027550909e964f14
Alexander Zarin <adamant545@gmail.com>
2023-11-19 13:49:11 +0400

    Create mm_threads_api.py

---
+++ Mattermost-API/mm_threads_api.py
@@ -0,0 +1,9 @@
+from typing import Union, List, Dict
+from Mattermost_Base import Base
+
+
+class Threads(Base):
+    def __init__(self, token: str, server_url: str):
+        super().__init__(token, server_url)
+        self.api_url = f"{self.base_url}/users"
+
