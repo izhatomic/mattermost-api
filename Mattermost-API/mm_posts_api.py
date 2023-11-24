9f8c53753b9dcde917001056cd6def356a6ea40b
Alexander Zarin <adamant545@gmail.com>
2023-11-24 20:34:43 +0400

    Define the get_posts_around_oldest_unread function

--- Mattermost-API/mm_posts_api.py
+++ Mattermost-API/mm_posts_api.py
@@ -343,3 +343,44 @@ class Posts(Base):
             self.add_to_json('include_deleted', include_deleted)
 
         return self.request(url, request_type='GET', body=True)
+
+    def get_posts_around_oldest_unread(self,
+                                       user_id:str,
+                                       channel_id:str,
+                                       limit_before:int,
+                                       limit_after:int,
+                                       skipFetchThreads:bool,
+                                       collapsedThreads:bool,
+                                       collapsedThreadsExtended:bool)->dict:
+        """
+        Get a page of posts in a channel. Use the query parameters to modify the behaviour of this endpoint. The parameter since must not be used with any of before, after, page, and per_page parameters. If since is used, it will always return all posts modified since that time, ordered by their create time limited till 1000. A caveat with this parameter is that there is no guarantee that the returned posts will be consecutive. It is left to the clients to maintain state and fill any missing holes in the post order.
+
+        Must have read_channel permission for the channel.
+
+        :param user_id: ID of the user
+        :param channel_id: The channel ID to get the posts for
+        :param limit_before: Default: 60. The number of posts per page
+        :param limit_after: Provide a non-zero value in Unix time milliseconds to select posts modified after that time
+        :param skipFetchThreads: A post id to select the posts that came before this one
+        :param collapsedThreads: A post id to select the posts that came after this one
+        :param collapsedThreadsExtended: ID of the post
+        :return: Post list retrieval info
+        """
+
+        url = f"{self.base_url}/users/{user_id}/channels/{channel_id}/posts/unread"
+
+        self.reset()
+        self.add_application_json_header()
+        if limit_before is not None:
+            self.add_to_json('limit_before', limit_before)
+        if limit_after is not None:
+            self.add_to_json('limit_after', limit_after)
+        if skipFetchThreads is not None:
+            self.add_to_json('skipFetchThreads', skipFetchThreads)
+        if collapsedThreads is not None:
+            self.add_to_json('collapsedThreads', collapsedThreads)
+        if collapsedThreadsExtended is not None:
+            self.add_to_json('collapsedThreadsExtended', collapsedThreadsExtended)
+
+        return self.request(url, request_type='GET', body=True)
+
