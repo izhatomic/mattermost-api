58c68d2a584e8917fa24e41945e3bce09c762280
Alexander Zarin <adamant545@gmail.com>
2023-11-25 19:25:06 +0400

    Define the autologin_to_mm_server_using_cws_token function

--- Mattermost-API/mm_users_api.py
+++ Mattermost-API/mm_users_api.py
@@ -46,3 +46,27 @@ class Uploads(Base):
             self.add_to_json('password', password)
 
         return self.request(url, request_type='POST', body=True)
+
+    def autologin_to_mm_server_using_cws_token(self,
+                                               login_id: str = None,
+                                               cws_token: str = None) -> dict:
+        """
+        CWS stands for Customer Web Server which is the cloud service used to manage cloud instances.
+
+        A Cloud license is required
+
+        :param login_id: User login_id for authentication.
+        :param cws_token: User cws_token for authentication.
+        :return: User login info.
+        """
+
+        url = f"{self.api_url}/login/cws"
+
+        self.reset()
+        self.add_application_json_header()
+        if login_id is not None:
+            self.add_to_json('login_id', login_id)
+        if cws_token is not None:
+            self.add_to_json('cws_token', cws_token)
+
+        return self.request(url, request_type='POST', body=True)
