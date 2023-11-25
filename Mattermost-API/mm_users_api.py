8a0ce79c71afb5752784ebacbaf915086d4686a5
Alexander Zarin <adamant545@gmail.com>
2023-11-25 20:03:12 +0400

    Define the create_user function

--- Mattermost-API/mm_users_api.py
+++ Mattermost-API/mm_users_api.py
@@ -85,3 +85,71 @@ class Uploads(Base):
         self.reset()
 
         return self.request(url, request_type='POST')
+
+    def create_user(self,
+                    email: str,
+                    username: str,
+                    t: str = None,
+                    iid: str = None,
+                    first_name: str = None,
+                    last_name: str = None,
+                    nickname: str = None,
+                    auth_data: str = None,
+                    auth_service: str = None,
+                    password: str = None,
+                    locale: str = None,
+                    props: dict = None,
+                    notify_props: str = None) -> dict:
+        """
+        Create a new user on the system. Password is required for email login.
+        For other authentication types such as LDAP or SAML, auth_data and auth_service fields are required.
+
+        No permission required for creating email/username accounts on an open server.
+        Auth Token is required for other authentication types such as LDAP or SAML.
+
+        :param email: User email to be created.
+        :param username: User username to be created.
+        :param t: Token id from an email invitation.
+        :param iid: Token id from an invitation link.
+        :param first_name: User first_name to be created.
+        :param last_name: User last_name to be created.
+        :param nickname: User nickname to be created.
+        :param auth_data: User auth_data to be created.
+        :param auth_service: User auth_service to be created.
+        :param password: User password to be created.
+        :param locale: User locale to be created.
+        :param props: User props to be created.
+        :param notify_props: User notify_props to be created.
+        :return: User creation info.
+        """
+
+        url = f"{self.api_url}"
+
+        self.reset()
+        self.add_application_json_header()
+        self.add_to_json('email', email)
+        self.add_to_json('username', username)
+        if t is not None:
+            self.add_to_json('t', t)
+        if iid is not None:
+            self.add_to_json('iid', iid)
+        if first_name is not None:
+            self.add_to_json('first_name', first_name)
+        if last_name is not None:
+            self.add_to_json('last_name', last_name)
+        if nickname is not None:
+            self.add_to_json('nickname', nickname)
+        if auth_data is not None:
+            self.add_to_json('auth_data', auth_data)
+        if auth_service is not None:
+            self.add_to_json('auth_service', auth_service)
+        if password is not None:
+            self.add_to_json('password', password)
+        if locale is not None:
+            self.add_to_json('locale', locale)
+        if props is not None:
+            self.add_to_json('props', props)
+        if notify_props is not None:
+            self.add_to_json('notify_props', notify_props)
+
+        return self.request(url, request_type='POST', body=True)
