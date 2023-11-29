415c686c02f932fdcb25b2a2442e2282a0bc0cc0
Alexander Zarin <adamant545@gmail.com>
2023-11-30 00:06:38 +0400

    Define the record_user_action_when_they_accept_or_decline_custom_tos function

--- Mattermost-API/mm_users_api.py
+++ Mattermost-API/mm_users_api.py
@@ -1409,4 +1409,29 @@ class Uploads(Base):
 
         return self.request(url, request_type='PUT', body=True)
 
+    def record_user_action_when_they_accept_or_decline_custom_tos(self,
+                                                                  user_id: str,
+                                                                  serviceTermsId: str,
+                                                                  accepted: str) -> dict:
+        """
+        Records user action when they accept or decline custom terms of service. Records the action in audit table.
+        Updates user's last accepted terms of service ID if they accepted it.
 
+        Minimum server version: 5.4
+
+        Must be logged in as the user being acted on.
+
+        :param user_id: User GUID
+        :param serviceTermsId: Service-specific authentication data
+        :param accepted: true or false, indicates whether the user accepted or rejected the terms of service.
+        :return: TOS record info.
+        """
+
+        url = f"{self.api_url}/{user_id}/terms_of_service"
+
+        self.reset()
+        self.add_application_json_header()
+        self.add_to_json('serviceTermsId', serviceTermsId)
+        self.add_to_json('accepted', accepted)
+
+        return self.request(url, request_type='POST', body=True)
